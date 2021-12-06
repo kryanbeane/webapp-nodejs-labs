1146bd01898abc1d6f7724bd185f63cca9718725
bryankeane0 <20089166@mail.wit.ie>
2021-12-06 11:24:33 +0000

    Topic 10 - Salt and Hash

--- MoviesAPI/seedData/index.js
+++ MoviesAPI/seedData/index.js
@@ -11,10 +11,9 @@ async function loadUsers() {
   console.log('load user Data');
   try {
     await userModel.deleteMany();
-    await userModel.collection.insertMany(users);
+    await users.forEach(user => userModel.create(user));
     console.info(`${users.length} users were successfully stored.`);
-  } 
-  catch (err) {
+  } catch (err) {
     console.error(`failed to Load user Data: ${err}`);
   }
 }
