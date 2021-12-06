0f2dda9b30b00124e612207fe5cf7447c2b553ae
bryankeane0 <20089166@mail.wit.ie>
2021-12-06 12:42:32 +0000

    Topic 10 - Exercises

--- MoviesAPI/api/users/index.js
+++ MoviesAPI/api/users/index.js
@@ -4,7 +4,7 @@ import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
 import movieModel from '../movies/movieModel';
 
-const router = express.Router(); // eslint-disable-line
+const router = express.Router();
 
 // Get all users
 router.get('/', async (req, res) => {
@@ -15,22 +15,35 @@ router.get('/', async (req, res) => {
 // Register OR authenticate a user
 router.post('/',asyncHandler( async (req, res, next) => {
     if (!req.body.username || !req.body.password) {
+
       res.status(401).json({success: false, msg: 'Please pass username and password.'});
       return next();
     }
     if (req.query.action === 'register') {
-      await User.create(req.body);
+
+      if(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/.test(req.body.password)) {
+
+        await User.create(req.body).catch(next);
+      } 
+      else {
+        res.status(401).json({code: 401,msg: 'Authentication failed. Password to weak'});
+      }
       res.status(201).json({code: 201, msg: 'Successful created new user.'});
-    } else {
+    } 
+    else {
+
       const user = await User.findByUserName(req.body.username);
-        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+        if (!user) return res.status(401).json( {
+           code: 401, msg: 'Authentication failed. User not found.' 
+        } );
         user.comparePassword(req.body.password, (err, isMatch) => {
           if (isMatch && !err) {
-            // if user is found and password matches, create a token
+
             const token = jwt.sign(user.username, process.env.SECRET);
-            // return the information including token as JSON
+
             res.status(200).json({success: true, token: 'BEARER ' + token});
-          } else {
+          } 
+          else {
             res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
           }
         });
@@ -56,7 +69,8 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
   const userName = req.params.userName;
   const movie = await movieModel.findByMovieDBId(newFavourite);
   const user = await User.findByUserName(userName);
-  await user.favourites.push(movie._id);
+  if(await user.favourites.includes(movie.id))
+    await user.favourites.push(movie._id);
   await user.save(); 
   res.status(201).json(user); 
 }));
