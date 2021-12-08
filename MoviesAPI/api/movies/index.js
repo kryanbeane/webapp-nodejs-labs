41038eb7dcd93fa2d201b3bda428683310c98eaf
bryankeane0 <20089166@mail.wit.ie>
2021-12-08 14:14:29 +0000

    Topic 10 - Mistake adding movieModel.js and movie.js. Now added so app now runs as intended.

--- MoviesAPI/api/movies/index.js
+++ MoviesAPI/api/movies/index.js
@@ -5,8 +5,18 @@ import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 
 router.get('/', asyncHandler(async (req, res) => {
-    const movies = await movieModel.find();
-    res.status(200).json(movies);
+    let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
+    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+
+    const totalDocumentsPromise = movieModel.estimatedDocumentCount(); //Kick off async calls
+    const moviesPromise = movieModel.find().limit(limit).skip((page - 1) * limit);
+
+    const totalDocuments = await totalDocumentsPromise; //wait for the above promises to be fulfilled
+    const movies = await moviesPromise;
+
+    const returnObject = { page: page, total_pages: Math.ceil(totalDocuments / limit), total_results: totalDocuments, results: movies };//construct return Object and insert into response object
+
+    res.status(200).json(returnObject);
 }));
 
 // Get movie details
